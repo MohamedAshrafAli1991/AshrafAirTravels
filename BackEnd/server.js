6f0bf48c2573d30af74dfdd864f9fae40d7def95
Sivam94 <paramasivamvicky@gmail.com>
2020-03-25 13:28:40 +0530

    test commit

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 const DB_STRING = process.env.DB_CONNECTION_STRING;
 
 //DB Connection
-mongoose.connect(DB_STRING, { useNewUrlParser: true,  useUnifiedTopology: true}, (err)=> {if(err) {console.log(err)} console.log(`MongoDB Connected`)});
+mongoose.connect(DB_STRING, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => { if (err) { console.log(err) } console.log(`MongoDB Connected`) });
 
 //middlewares
 app.use(express.json());
@@ -29,7 +29,7 @@ app.post('/api/addRole', (req, res) => {
     const role = new Role(data);
 
     role.save((err, doc) => {
-        if(err){
+        if (err) {
             console.log(err);
         }
         console.log('document saved');
@@ -45,7 +45,7 @@ app.post('/api/addUser', (req, res) => {
     const user = new User(data);
 
     user.save((err, doc) => {
-        if(err){
+        if (err) {
             console.log(err);
         }
         console.log('document saved');
@@ -57,9 +57,9 @@ app.post('/api/addUser', (req, res) => {
 //GetUser
 app.get('/api/user/:token', (req, res) => {
     const id = req.params.token;
-    
-    const user = User.findOne({token: id}, (err, user) => {
-        if(err){
+
+    const user = User.findOne({ token: id }, (err, user) => {
+        if (err) {
             console.log(err);
         }
         console.log(user);
@@ -69,3 +69,5 @@ app.get('/api/user/:token', (req, res) => {
 
 //Server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+//test change
